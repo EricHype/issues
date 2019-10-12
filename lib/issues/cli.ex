f086ef03534358edade07f885e931fd506b8e414
Eric Heitmuller <eheitmuller@squareup.com>
2019-10-11 17:24:26 -0700

    Adding hex and HTTPoison dependency

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -7,7 +7,9 @@ defmodule Issues.CLI do
     """
 
     def run(argv) do
-        parse_args(argv)
+        argv
+        |> parse_args
+        |> process
     end
 
     @doc """
@@ -17,12 +19,35 @@ defmodule Issues.CLI do
     returns a tuple of {user,project,count} or `:help`
     """
     def parse_args(argv) do
-        parse = OptionParser.parse(argv, switches: [help: :boolean], aliases: [h: :help])
-        case parse do
-            {[help: true], _, _} -> :help
-            {_, [user, project, count], _ } -> {user, project, String.to_integer(count) }
-            {_, [user, project], _} -> {user, project, @default_count}
-            _ -> :help
-        end 
+        # parse = OptionParser.parse(argv, switches: [help: :boolean], aliases: [h: :help])
+        # case parse do
+        #     {[help: true], _, _} -> :help
+        #     {_, [user, project, count], _ } -> {user, project, String.to_integer(count) }
+        #     {_, [user, project], _} -> {user, project, @default_count}
+        #     _ -> :help
+        # end 
+        OptionParser.parse(argv, switches: [help: :boolean], aliases: [h: :help])
+        |> elem(1)
+        |> args_to_internal_representation()
     end
+
+    def args_to_internal_representation([user, project, count]) do
+        {user, project, String.to_integer(count) }
+    end
+    def args_to_internal_representation([user, project]) do
+        {user, project, @default_count }
+    end
+    def args_to_internal_representation(_) do
+        :help
+    end
+
+    def process(:help) do
+        IO.puts """ 
+        usage: issues <user> <project> [count | #{@default_count}]
+        """
+        System.halt(0)
+    end
+    def process({user, project, _count}) do
+        Issues.GithubIssues.fecth(user, project)
+    end  
 end
