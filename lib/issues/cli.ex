62383cbae872315859b24f78cc1c4c10a9a0e22a
Eric Heitmuller <eheitmuller@squareup.com>
2019-10-14 15:52:15 -0700

    refactor to use Poison for JSON

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -49,5 +49,12 @@ defmodule Issues.CLI do
     end
     def process({user, project, _count}) do
         Issues.GithubIssues.fecth(user, project)
+        |> decode_response()
     end  
+
+    def decode_response({:ok, body}), do: body
+    def decode_response({:error, error}) do
+        IO.puts "Error fetching from github: #{error["message"]}"
+        System.halt(2)
+    end
 end
