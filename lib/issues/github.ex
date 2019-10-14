59023a4ced280d07ba976b60eb348731cce4c6e8
Eric Heitmuller <eheitmuller@squareup.com>
2019-10-14 16:22:44 -0700

    Last x function complete

--- lib/issues/github.ex
+++ lib/issues/github.ex
@@ -1,8 +1,8 @@
 defmodule Issues.GithubIssues do
-  @user_agent[{"User-Agent", "username@gmail.com"}]
-
+  # @user_agent[{"User-Agent", "username@gmail.com"}]
+  @github_url Application.get_env(:issues, :github_url)
   def issues_url(user, project) do
-    "https://api.github.com/repos/#{user}/#{project}/issues"
+    "#{@github_url}/repos/#{user}/#{project}/issues"
   end
   
   def fetch(user, project) do
